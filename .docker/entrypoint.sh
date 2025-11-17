ce8c603ca20a90c4a2c159509b375570fc272619
Maxence Rose <pirmax@icloud.com>
2025-11-17 16:39:01 +0100

    Fix wp-config copy path in entrypoint script to use correct filename

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,8 +2,9 @@
 
 # Copy wp-config if it doesn't exist
 if [ ! -f /var/www/html/wp-config-docker.php ]; then
-    cp /tmp/wp-config-docker.php /var/www/html/wp-config-docker.php
-    chown www-data:www-data /var/www/html/wp-config-docker.php
+    cp /tmp/wp-config-docker.php /var/www/html/wp-config.php
+    chown www-data:www-data /var/www/html/wp-config.php
+    chmod 400 /var/www/html/wp-config.php
 fi
 
 # Execute CMD
